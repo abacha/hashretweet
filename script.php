771382324b7b9a805ca72b6ef81fb3f2a29a0ea3
Adriano Bacha <abacha@gmail.com>
2010-07-31 16:21:46 -0400

    Output modified when a post is sucessfull

--- script.php
+++ script.php
@@ -2,17 +2,17 @@
 <?php
 if (!file_exists("config.php")) die("FILE config.php NOT FOUND");
 include "config.php";
-$last_id = file_get_contents(getcwd().'/last_id');
-$json = file_get_contents("http://search.twitter.com/search.json?tag=jera&rpp=100&since_id=".$last_id);
-$dados = json_decode($json);
+$dados = json_decode(file_get_contents("http://search.twitter.com/search.json?tag=jera&rpp=100&since_id=".file_get_contents(getcwd().'/last_id')));
 $tweets = array_reverse($dados->results);
-echo sizeof($tweets).' tweets found!';
+echo "\n".sizeof($tweets).' tweets found!';
 if (!empty($tweets)) {
-	foreach ($tweets as $k => $tweet) {		
-		if (in_array($tweet->from_user_id, $white_list) && substr($tweet->text, 0, 3) != "RT ")
-			tweet($tweet->from_user, $tweet->text);
-		else if ($tweet->from_user_id != 126665619) 
-			error_log("\n[@{$tweet->from_user}] {$tweet->text} ({$tweet->created_at})", 3, 'posts.log');
+	foreach ($tweets as $k => $tweet) {
+		 if (substr($tweet->text, 0, 3) != "RT ") {
+			if (in_array($tweet->from_user_id, $white_list))
+				tweet($tweet->from_user, $tweet->text);
+			else if ($tweet->from_user_id != 126665619) 
+				error_log("\n[@{$tweet->from_user}] {$tweet->text} ({$tweet->created_at})", 3, 'posts.log');
+		}
 	}
 	$last = array_pop($tweets);
 	$last_id = $last->id;
@@ -26,20 +26,21 @@ function tweet($user, $status) {
 	$status = "RT @".$user.": ".$status;	
 	$posted = 0;
 	$curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "$tweetUrl");
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, "status=$status");
-        curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+    curl_setopt($curl, CURLOPT_URL, $tweetUrl);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, "status=$status");
+    curl_setopt($curl, CURLOPT_USERPWD, USERNAME.":".PASSWORD);
 
-	while ($posted == 0) {
-		echo $posted;
+	while ($posted == 0) {		
 		$result = curl_exec($curl);
 		$resultArray = curl_getinfo($curl);
 		if ($resultArray['http_code'] == 200) $posted = 1;
+		else print_r($resultArray);
 		sleep(3);
 	}
+	echo "\nPosted successfully!";
 	curl_close($curl);
 	sleep(3);
 }
